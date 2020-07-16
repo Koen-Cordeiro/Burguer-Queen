20621a01ddf65668cfa71563a8ce994068e3bd51
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-16 19:27:20 -0300

    Register com css base configurado

--- src/Pages/register.js
+++ src/Pages/register.js
@@ -1,14 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { StyleSheet, css } from 'aphrodite';
+import Input from '../Components/input'
 
-const Register = () => (
-    <form>
-    <input type='text'/>
-    <input type='email'/>
-    <input type='password'/>
-    <input type='password'/>
-    <Link to='/' exact={true}><button>Registre-se</button></Link> 
-    </form>
-)
+const Register = () => {
+
+    const arrText = [ 
+        {text: "Digite seu nome", type: "text"},
+        {text: "Digite seu e-mail", type: "email"},
+        {text: "Digite sua senha", type: "password"},
+        {text: "Confirme sua senha", type: "password"},
+    ]
+  return ( <form className={css(styles.flexCenter)}>
+    {arrText.map((e, index)=> <Input key={index} type={e.type}text={e.text}/>)}
+    <button className={css(styles.red)}>Registre-se</button>
+    </form>)
+}
+const styles = StyleSheet.create({
+    flexCenter: {
+        display: "flex",
+        flexDirection: "column"
+    },
+    red: {
+        backgroundColor: 'red'
+    },
+
+    blue: {
+        backgroundColor: 'blue'
+    },
+
+    hover: {
+        ':hover': {
+            backgroundColor: 'red'
+        }
+    },
+
+    small: {
+        '@media (max-width: 600px)': {
+            backgroundColor: 'red',
+        }
+    }
+});
 
 export default Register
