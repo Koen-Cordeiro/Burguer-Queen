0cf8b67a2ff9861d4167b63acb8cd538cca1fd2e
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-17 16:16:44 -0300

    Correção do registro

--- src/Pages/register.js
+++ src/Pages/register.js
@@ -1,45 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { StyleSheet, css } from 'aphrodite';
 import Input from '../Components/input'
 
+
 const Register = () => {
 
-    const arrText = [ 
-        {text: "Digite seu nome", type: "text"},
-        {text: "Digite seu e-mail", type: "email"},
-        {text: "Digite sua senha", type: "password"},
-        {text: "Confirme sua senha", type: "password"},
-    ]
-  return ( <form className={css(styles.flexCenter)}>
-    {arrText.map((e, index)=> <Input key={index} type={e.type}text={e.text}/>)}
+  const arrText = [
+    { text: "Digite seu nome", type: "text" },
+    { text: "Digite seu e-mail", type: "email" },
+    { text: "Digite sua senha", type: "password" },
+    { text: "Confirme sua senha", type: "password" },
+  ]
+  return (<form className={css(styles.flexCenter)}>
+    {arrText.map((e, index) => <Input key={index} type={e.type} text={e.text} />)}
     <button className={css(styles.red)}>Registre-se</button>
-    </form>)
+  </form>)
 }
 const styles = StyleSheet.create({
-    flexCenter: {
-        display: "flex",
-        flexDirection: "column"
-    },
-    red: {
-        backgroundColor: 'red'
-    },
-
-    blue: {
-        backgroundColor: 'blue'
-    },
-
-    hover: {
-        ':hover': {
-            backgroundColor: 'red'
-        }
-    },
-
-    small: {
-        '@media (max-width: 600px)': {
-            backgroundColor: 'red',
-        }
-    }
+  flexCenter: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  red: {
+    backgroundColor: 'red'
+  },
 });
 
 export default Register
