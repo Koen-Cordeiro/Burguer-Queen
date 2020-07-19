7e753ad699106acd837baf6fb4772f58e1bd5579
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-19 13:17:15 -0300

    Caminho do componente register alterado

--- src/Components/input.js
+++ src/Components/input.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Input = (props) => (
   <>
     <label>{props.text}</label>
-    <input className={props.inputClass} type={props.type} />
+    <input value={props.value} className={props.inputClass} type={props.type} />
   </>
 )
 
