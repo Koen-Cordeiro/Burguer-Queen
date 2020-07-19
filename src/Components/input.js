fc6be6ffb6af8ebbba753c896d16c3a08df6162c
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-19 19:57:41 -0300

    Propriedade de name criada no componente

--- src/Components/input.js
+++ src/Components/input.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Input = (props) => (
   <>
     <label>{props.text}</label>
-    <input value={props.value} className={props.inputClass} onChange={props.handleChange} type={props.type} />
+    <input value={props.value} name={props.name} className={props.inputClass} onChange={props.handleChange} type={props.type} />
   </>
 )
 
