24e3d72733ce1b5bbae5ed3a8563cb0a8ecc2b1c
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-07-20 13:29:05 -0300

    firebase login criado

--- src/Pages/login/login.js
+++ src/Pages/login/login.js
@@ -4,11 +4,27 @@ import Input from "../Components/input";
 
 const Login = () => {
   
+  
+
+  const login = (staff) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(staff.email, staff.password)
+      .then((user) => {
+        callback(user);
+        checkUser(user);
+      })
+      .catch((error) => {
+        callback(error);
+      });
+    };
+  }
+
   const arrText = [
     { text: "E-mail", type: "email" },
     { text: "Senha", type: "password" },
   ];
-
+  
   return (
     <main>
       <section>
@@ -39,11 +55,10 @@ const toggleSignIn = ({ email, password }, callback) => {
     .catch((error) => {
       callback(error);
     });
-};
 
 // check if user exists on firebase salão & cozinha
 const checkUser = (user) => {
-  const load = firebase.firestore().collection("users").doc(user.uid);
+  const load = firebase.firestore().collection("users-info").doc(user.uid);
   load.get().then((doc) => {
     if (!doc.exists) {
       newUser(user);
