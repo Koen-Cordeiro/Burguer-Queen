c5667b22e03da9bc05f589d45d8eee9eb5a81976
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-07-20 13:14:51 -0300

    componente login criado

---
+++ src/Pages/login/login.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Input from "../Components/input";
+
+const Login = () => {
+  
+  const arrText = [
+    { text: "E-mail", type: "email" },
+    { text: "Senha", type: "password" },
+  ];
+
+  return (
+    <main>
+      <section>
+        <h1>Burguer Queen</h1>
+        <form className={}>
+          {arrText.map((e, index) => (
+            <Input key={index} text={e.text} type={e.type} />
+          ))}
+          <Link to="MAP PRA RECEBER COZINHA OU SALÃO">
+            <button>Login</button>
+          </Link>
+        </form>
+      </section>
+      <section>Lateral</section>
+    </main>
+  );
+};
+
+// signin email firebase
+const toggleSignIn = ({ email, password }, callback) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      callback(user);
+      checkUser(user);
+    })
+    .catch((error) => {
+      callback(error);
+    });
+};
+
+// check if user exists on firebase salão & cozinha
+const checkUser = (user) => {
+  const load = firebase.firestore().collection("users").doc(user.uid);
+  load.get().then((doc) => {
+    if (!doc.exists) {
+      newUser(user);
+    }
+  });
+};
+
+export default Login;
