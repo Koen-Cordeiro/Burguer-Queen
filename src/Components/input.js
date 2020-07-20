65e195f68c50ef01de78f91b8664aac81a05f9de
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-20 13:06:46 -0300

    Input agora fica dentro de uma label

--- src/Components/input.js
+++ src/Components/input.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 const Input = (props) => (
-  <>
-    <label>{props.text}</label>
+    <label>{props.text}
     <input value={props.value} name={props.name} className={props.inputClass} onChange={props.handleChange} type={props.type} />
-  </>
+    </label>
 )
 
 export default Input
