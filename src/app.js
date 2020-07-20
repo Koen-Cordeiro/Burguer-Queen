9d6ed3a7a4edfeabbdbaacc919bcd36c731e734f
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-20 17:57:27 -0300

    Rota para o usuário deslogado criada

---
+++ src/app.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import firebase from 'firebase'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Login from '../src/Pages/login/login'
+import Register from '../src/Pages/register/register'
+import Page404 from '../src/Pages/404'
+
+const App = () => {
+
+  const [userPage, setUserPage] = useState();
+
+  const init = () => {
+
+  }
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        
+
+      } else {
+        console.log('to deslogado')
+        setUserPage(() => <BrowserRouter>
+          <Switch>
+            <Route path="/" exact={true} component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path='*' component={Page404} />
+          </Switch>
+        </BrowserRouter>)
+      }
+    });
+
+  }, [])
+
+
+
+
+  // const checkWorkPlace = (user) => {
+  //   const userCollection = firebase.firestore().collection("users-info").doc(user.uid);
+  //   userCollection.get().then((staff) => {
+  //     if (staff.workPlace === "cozinha") {
+  //     // vai para cozinha
+  //     }
+  //     if (staff.workPlace === "salão") {
+  //       // vai para salão
+  //     }
+  //   });
+  // }
+
+
+
+  return (
+    <>
+      {userPage}
+    </>);
+
+};
+
+export default App;
