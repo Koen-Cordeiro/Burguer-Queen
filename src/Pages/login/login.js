7ced3fd27206c43becb79c3af3500d0496c41344
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-22 09:32:06 -0300

    css login básico criado e importado no arquivo principal

--- src/Pages/login/login.js
+++ src/Pages/login/login.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
 import firebase from 'firebase'
 import { Link } from "react-router-dom";
+import Button from '../../Components/button'
 import logo from '../../img/logo.png'
 import Input from '../../Components/input'
+import './loginStyle.css'
 
 const Login = () => {
-  
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
+
   const login = (staff) => {
     firebase
       .auth()
@@ -18,29 +20,32 @@ const Login = () => {
       })
       .catch((error) => {
         // callback(error);
-    });
+      });
   }
 
   const arrText = [
-    { text: "E-mail", type: "email", value: email, handleChange: (e) => setEmail(e.currentTarget.value)},
-    { text: "Senha", type: "password", value: password, handleChange: (e) => setPassword(e.currentTarget.value)},
+    { text: "E-mail", type: "email", value: email, handleChange: (e) => setEmail(e.currentTarget.value) },
+    { text: "Senha", type: "password", value: password, handleChange: (e) => setPassword(e.currentTarget.value) },
   ];
 
   return (
-      <section className='flex-center'>
-        <img alt='logo chase burguer' src={logo}/>
-        <form className='flex-column center border-form'>
+    <>
+      <figure>
+        <img alt='logo chase burguer' src={logo} />
+      </figure>
+      <section className='flex-center border-form'>
+        <form className='flex-column center'>
           {arrText.map((e, index) => (
             <Input key={index} componentClass='flex-column' text={e.text} type={e.type} value={e.value} handleChange={e.handleChange} />
           ))}
-          <button type="submit" onClick={(e) => {
+          <Button buttonClass='button' type='submit' text='Registre-se' handleClick={(e) => {
             e.preventDefault()
-            login({email, password})}}>
-            Entrar
-          </button>
+            login({ email, password })
+          }}/>
         </form>
-        <p className='change-register-login text-center'>Não possui uma conta?<br/><Link to='/register'>Registre-se</Link></p>
+          <p className='change-register-login text-center'>Não possui uma conta?<br /><Link to='/register'>Registre-se</Link></p>
       </section>
+      </>
   );
 
 };
