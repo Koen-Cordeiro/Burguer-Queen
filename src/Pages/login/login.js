0c55ebc4591be110dc78ef36c16280d8ae877692
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-24 16:03:33 -0300

    Correção para o arquivo funcionar]

--- src/Pages/login/login.js
+++ src/Pages/login/login.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import Form from '../../Components/form'
 import Button from '../../Components/button'
 import logo from '../../img/logo.png'
-import './loginStyle.css'
 import { errorsLogin } from './errorsLogin'
 import Input from '../../Components/input'
 
