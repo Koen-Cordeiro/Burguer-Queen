0e7d1ae1fd7f0b92ee0cd068f37cd839789cb8d2
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-27 14:24:52 -0300

    Input de mesa e cliente criados e pedidos colocados dentro de uma div

--- src/Pages/saloon/saloon.js
+++ src/Pages/saloon/saloon.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Button from '../../Components/button'
+import Input from '../../Components/input'
 import MenuItems from '../../Components/dishes'
 import firebase from 'firebase'
 
@@ -31,7 +32,7 @@ const Saloon = () => {
     setOrder([...order, arr[1].innerText])
 
   }
-  useEffect(()=> setOrderNumber((Math.random() * 100000).toFixed(0)), [])
+  useEffect(() => setOrderNumber((Math.random() * 100000).toFixed(0)), [])
   useEffect(() => {
     let countTypes = order.reduce(function (allTypes, atualType) {
       if (atualType in allTypes) {
@@ -58,8 +59,11 @@ const Saloon = () => {
 
   return (
     <div className='menu-row-reverse'>
-      <form>
+      <form className='log-reg-flex'>
         <h1>Pedido {orderNumber}</h1>
+        <Input type='text' text='Nome do Cliente' />
+        <Input type='number' text='Mesa' />
+        <div>
           {menu.map((e, index) => (
             <div key={index}>
               <h2 key={e.type}>{e.type}</h2>
@@ -67,20 +71,22 @@ const Saloon = () => {
               <h2 key={e.count + e.type}>{e.count}</h2>
             </div>
           ))}
-        </form>
+        </div>
+
+      </form>
       <section >
 
-      <h1>Bem vindo senhor das mesas</h1>
-      <Button handleClick={() => firebase.auth().signOut()} text='Sair'/> 
+        <h1>Bem vindo senhor das mesas</h1>
+        <Button handleClick={() => firebase.auth().signOut()} text='Sair' />
 
-      <Button text='Café da manhã' handleClick={() => {
-        setBreakfastClick(!breakfastClick)
-        setAllDayClick(false)
-      }} />
-      <Button text='Resto do dia' handleClick={() => {
-        setAllDayClick(!alldayClick)
-        setBreakfastClick(false)
-      }} />
+        <Button text='Café da manhã' handleClick={() => {
+          setBreakfastClick(!breakfastClick)
+          setAllDayClick(false)
+        }} />
+        <Button text='Resto do dia' handleClick={() => {
+          setAllDayClick(!alldayClick)
+          setBreakfastClick(false)
+        }} />
 
         <ul>
           {breakfastClick && <MenuItems arr={breakfast} handleClick={addOrder} />}
