7514f57d41a0126b036387caf783a4d06feb6371
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-28 18:28:48 -0300

    Estilização do componente button criado

--- src/Components/button/button.js
+++ src/Components/button/button.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './button.scss'
 
-const Button = (props) => (
-  <button className={props.buttonClass} onClick={props.handleClick} type={props.type}>
+const Button = (props) => {
+  const classButton = `button-${props.type}`
+  return(<button className={props.className} onClick={props.handleClick} type={props.type}>
     {props.text}
-  </button>
-)
+  </button>)
+}
+
+
 
 export default Button
