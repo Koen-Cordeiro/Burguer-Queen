99365b2778267da6f5832aaef8cbda0a9c4d0970
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-28 11:03:07 -0300

    Página de pedidos criada

--- src/Pages/saloon/orders.js
+++ src/Pages/saloon/orders.js
@@ -4,14 +4,10 @@ import Button from '../../Components/button'
 
 
 const Orders = () => {
-  const [menu, setMenu] = useState(true)
-
-
 
   return (
     <>
-      <Button text='Menu' handleClick={() => setMenu(!menu)} />
-      {menu && <Menu />}
+      <h1>Oie eu sou a área de pedidos</h1>
     </>
   );
 };
