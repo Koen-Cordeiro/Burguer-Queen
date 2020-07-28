aa5d9a0a76aed3d54560842e2b04c92f8534818e
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-28 10:51:10 -0300

    Saloon agora recebe apenas o input menu

--- src/Pages/saloon/saloon.js
+++ src/Pages/saloon/saloon.js
@@ -1,116 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import Menu from './menu'
 import Button from '../../Components/button'
-import Input from '../../Components/input'
-import MenuItems from '../../Components/dishes'
-import firebase from 'firebase'
+
+
 
 const Saloon = () => {
-  const [breakfast, setBreakfast] = useState([])
-  const [clientOrder, setClientOrder] = useState([])
-  const [snacks, setSnacks] = useState([])
-  const [drinks, setDrinks] = useState([])
-  const [burguers, setBurguers] = useState([])
-  const [breakfastClick, setBreakfastClick] = useState(false)
-  const [alldayClick, setAllDayClick] = useState(true)
-  const [order, setOrder] = useState([])
-  const [orderNumber, setOrderNumber] = useState()
-  const [table, setTable] = useState('')
-  const [clientName, setClientName] = useState('')
-  const [finalPrice, setFinalPrice] = useState(0)
+  const [menu, setMenu] = useState(true)
 
-  const requestData = (document) => {
-    firebase.firestore().collection('menu').doc(document.menu).collection(document.type).get().then((snap => {
-      const getBurguer = snap.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data()
-      }))
-      document.set(() => getBurguer);
-    })
-    )
-  }
 
-  const addOrder = (event) => {
-    const arr = Array.from(event.currentTarget.children)
-    setOrder([...order, { type: arr[1].innerText, price: Number(arr[0].innerText) }])
-  }
-
-  const sendOrder = (post) => {
-    firebase.firestore().collection('orders').doc(post.orderNumber).set(post)
-  }
-
-  useEffect(() => setOrderNumber((Math.random() * 100000).toFixed(0)), [])
-  useEffect(() => {
-    setClientOrder(order.reduce(function (allTypes, atualType) {
-      const index = allTypes.findIndex(x => x.type === atualType.type)
-      if (index !== -1) {
-        allTypes[index].count++
-      } else {
-        allTypes.push({ type: atualType.type, price: atualType.price, count: 1 })
-      }
-      return allTypes;
-    }, []))
-  }, [order])
-  useEffect(() => setFinalPrice(clientOrder.reduce((allTypes, atualType) => atualType.price * atualType.count+allTypes, 0 )), [clientOrder])
-  useEffect(() => requestData({ menu: 'Breakfast', type: 'pratos', set: setBreakfast }), [])
-  useEffect(() => requestData({ menu: 'All-day', type: 'acompanhamentos', set: setSnacks }), [])
-  useEffect(() => requestData({ menu: 'All-day', type: 'bebidas', set: setDrinks }), [])
-  useEffect(() => requestData({ menu: 'All-day', type: 'hamburgueres', set: setBurguers }), [])
-
-  const reloadData = (event, greater) => {
-    const element = event.currentTarget.parentElement.firstChild.textContent
-    const index = order.findIndex(x => x.type === element)
-    greater ? order.push(order[index]) : order.pop(order[index])
-    setOrder([...order])
-  }
 
   return (
-    <div className='menu-row-reverse'>
-      <form className='log-reg-flex'>
-        <h1>Pedido {orderNumber}</h1>
-        <Input type='text' text='Nome do Cliente' handleChange={(e) => setClientName(e.currentTarget.value)} />
-        <Input type='number' text='Mesa' handleChange={(e) => setTable(e.currentTarget.value)} />
-        <div>
-          {clientOrder.map((e, index) => (
-            <div key={index}>
-              <h2 key={e.type}>{e.type}</h2>
-              <h2 key={e.price}>{e.price}</h2>
-              <h3 onClick={(event) => reloadData(event, true)}>+</h3>
-              <h2 key={e.count + e.type}>{e.count}</h2>
-              <h3 onClick={(event) => reloadData(event, false)}>-</h3>
-            </div>
-          ))}
-          <h2>R${finalPrice}</h2> 
-
-          <Button text='Enviar pedido' handleClick={(event) => {
-            event.preventDefault()
-            const orderNumberValue = `${clientName}-${table}-${orderNumber}`
-            sendOrder({ orderNumber: orderNumberValue, finalPrice, clientName, table, clientOrder, orderStatus: 'pending', workerName: firebase.auth().currentUser.displayName })
-          }} />
-        </div>
-
-      </form>
-      <section >
-
-        <h1>Bem vindo senhor das mesas</h1>
-        <Button handleClick={() => firebase.auth().signOut()} text='Sair' />
-
-        <Button text='Café da manhã' handleClick={() => {
-          setBreakfastClick(!breakfastClick)
-          setAllDayClick(false)
-        }} />
-        <Button text='Resto do dia' handleClick={() => {
-          setAllDayClick(!alldayClick)
-          setBreakfastClick(false)
-        }} />
-
-        <ul>
-          {breakfastClick && <MenuItems arr={breakfast} handleClick={addOrder} />}
-          {alldayClick && <MenuItems text='Acompanhamentos' arr={snacks} handleClick={addOrder} />}
-          {alldayClick && <MenuItems text='Bebidas' arr={drinks} handleClick={addOrder} />}
-          {alldayClick && <MenuItems text='Hamburgueres' arr={burguers} handleClick={addOrder} />}
-        </ul>
-      </section>
-    </div>
+    <>
+      <Button text='Menu' handleClick={() => setMenu(!menu)} />
+      {menu && <Menu />}
+    </>
   );
 };
 
