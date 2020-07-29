af7ec2b0c1dd6e20aecbfb832ed19f3dd735d0cb
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-29 09:55:16 -0300

    Placeholder de confirmação de senha adicionado

--- src/Pages/register/register.js
+++ src/Pages/register/register.js
@@ -35,7 +35,7 @@ const Register = () => {
     { text: "Nome", type: "text", value: name, placeholder:"Seu nome de detetive", handleChange: (e) => setName(e.currentTarget.value) },
     { text: "E-mail", type: "email", value: email, placeholder:"detetive@exemplo.com.br", handleChange: (e) => setEmail(e.currentTarget.value) },
     { text: "Senha", type: "password", value: password, placeholder:"Cifra de até 6 caracteres", handleChange: (e) => setPassword(e.currentTarget.value) },
-    { text: "Confirme sua senha", type: "password", value: confirmPassword, handleChange: (e) => setConfirmPassword(e.currentTarget.value) },
+    { text: "Confirme sua senha", type: "password", value: confirmPassword, placeholder: 'Confirme sua cifra' , handleChange: (e) => setConfirmPassword(e.currentTarget.value) },
   ]
   return (
     <section className='log-reg-flex reg-background'>
