623f11a9ac09c48053d7ea2b7dd717e1b3ee9a54
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-29 09:27:30 -0300

    Reaproveitamento de classes do botão de submit criadas

--- src/Components/button/button.js
+++ src/Components/button/button.js
@@ -2,8 +2,7 @@ import React from 'react'
 import './button.scss'
 
 const Button = (props) => {
-  const classButton = `button-${props.type}`
-  return(<button className={`${classButton} ${props.buttonClass}`} onClick={props.handleClick} type={props.type}>
+  return(<button className={props.buttonClass} onClick={props.handleClick} type={props.type}>
     {props.text}
   </button>)
 }
