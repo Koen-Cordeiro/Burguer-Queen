f511fe5d3527054db543e9ceb7c194c5a8abb4fb
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-29 10:31:58 -0300

    Input radio com estilização separada no próprio arquivo scss

--- src/Components/input/input.js
+++ src/Components/input/input.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import './input.scss'
 
 const Input = (props) => {
-    const inputClass = `input-${props.use}`
-    
-return (
-        <label className={props.componentClass}>{props.text}
-        <input value={props.value} name={props.name} onChange={props.handleChange} type={props.type} placeholder={props.placeholder} required/>
-        {props.textRadio}
-        </label>
-)}
+  const inputClass = `input-${props.use}`
+
+  return (
+    <label className={props.componentClass}>{props.text}
+      <input value={props.value} name={props.name} onChange={props.handleChange} type={props.type} placeholder={props.placeholder} required />
+      {props.textRadio}
+    </label>
+  )
+}
 
 export default Input
