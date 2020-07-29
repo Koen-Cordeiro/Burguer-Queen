09c31cd320b6369ec47cdb5054121aa8ca78be60
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-29 09:56:29 -0300

    Input atualizado com Placeholder

--- src/Components/input/input.js
+++ src/Components/input/input.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Input = (props) => {
     const inputClass = `input-${props.use}`
     
-    return (
-    <label className={props.componentClass}>{props.text}
-    <input value={props.value} name={props.name} className={props.inputClass} onChange={props.handleChange} type={props.type} required/>
-    {props.textRadio}
-    </label>
+return (
+        <label className={props.componentClass}>{props.text}
+        <input value={props.value} name={props.name} className={props.inputClass} onChange={props.handleChange} type={props.type} placeholder={props.placeholder} required/>
+        {props.textRadio}
+        </label>
 )}
 
 export default Input
