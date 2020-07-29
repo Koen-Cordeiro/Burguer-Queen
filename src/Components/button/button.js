d06847ad63e0ba0d54b4f07f5139a260f6e29987
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-29 09:08:32 -0300

    Correção dos componentes de botão na página de registro e login

--- src/Components/button/button.js
+++ src/Components/button/button.js
@@ -3,7 +3,7 @@ import './button.scss'
 
 const Button = (props) => {
   const classButton = `button-${props.type}`
-  return(<button className={props.className} onClick={props.handleClick} type={props.type}>
+  return(<button className={`${classButton} ${props.buttonClass}`} onClick={props.handleClick} type={props.type}>
     {props.text}
   </button>)
 }
