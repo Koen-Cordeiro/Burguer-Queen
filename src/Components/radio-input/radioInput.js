5e5fa91ce91d124de60e01089b62bfea9bace280
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-29 14:24:56 -0300

    Classe da div corrigida

--- src/Components/radio-input/radioInput.js
+++ src/Components/radio-input/radioInput.js
@@ -5,7 +5,7 @@ import './radioinput.scss'
 const RadioInputArea = (props) => {
   return(
     <label className='reg-work-label log-reg-space-after'>{props.labelText}
-    <div onChange={event => props.setValue(event.target.value)} className='input-sign-radio'>
+    <div onChange={event => props.setValue(event.target.value)} className='input-radio'>
       <Input type='radio' value={props.valueFieldOne} name={'radio'} textRadio={props.textInputOne} />
       <p>ou</p>
       <Input type='radio' value={props.valueFieldTwo} name={'radio'} textRadio={props.textInputTwo} />
