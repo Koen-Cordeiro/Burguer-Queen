f9549b7e1d867f13d7388840e16c8f69d532036e
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-31 09:12:01 -0300

    Arquivo dishes teve seu nome alterado para menuItems e agora recebe imagem quando está montando

--- src/Components/menu-items/menu-items.js
+++ src/Components/menu-items/menu-items.js
@@ -5,6 +5,7 @@ const MenuItems = (props) => (
   <h1>{props.text}</h1>
       {props.arr.map((e,index) =>( 
     <div key={index} onClick={(e)=>props.handleClick(e)}>
+    <img src={e.img}/>
     <div key={e.price}>{e.price}</div>
     <div key={e.type} name='type'>{e.type}</div>
   </div>))}
