832f590ef3325510565febb76d1a59cfbec6832d
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-31 10:40:08 -0300

    Alt adicionado a imagem

--- src/Components/menu-items/menu-items.js
+++ src/Components/menu-items/menu-items.js
@@ -5,7 +5,7 @@ const MenuItems = (props) => (
   <h1>{props.text}</h1>
       {props.arr.map((e,index) =>( 
     <div key={index} onClick={(e)=>props.handleClick(e)}>
-    <img src={e.img}/>
+    <img src={e.img} alt='Imagem do item do menu'/>
     <p>R$<span key={e.price}>{e.price}</span></p>
     <div key={e.type} name='type'>{e.type}</div>
   </div>))}
