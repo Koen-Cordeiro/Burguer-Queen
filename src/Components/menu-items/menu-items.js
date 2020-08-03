4cca417fc7573843287448b5039626c06c33ca1d
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-08-03 18:30:23 -0300

    Estilização dos items do cardápio

--- src/Components/menu-items/menu-items.js
+++ src/Components/menu-items/menu-items.js
@@ -5,13 +5,17 @@ import './menu-items.scss'
 const MenuItems = (props) => (
   <li>
     <h2 className='title'>{props.text}</h2>
-    {props.arr.map((e,index) =>( 
-    <div key={index} onClick={props.handleClick}>
-    {props.text!== 'Bebidas' && <img src={e.img} alt='Imagem do item do menu'/>}
-    <p>R$<span key={e.price}>{e.price}</span></p>
-    <div key={e.type} name='type'>{e.type}</div>
-  </div>))}
-  {props.burguer && <BurguerArea burguerClick={props.burguerClick} checkbox={props.checkbox} setCheckbox={props.setCheckbox} setValue={props.setValue}/>}
+    <ul>
+      <li className='list'>
+        {props.arr.map((e,index) =>( 
+          <div className='item' key={index} onClick={props.handleClick}>
+          <p>R${"\u00a0"}<span key={e.price}>{e.price}</span></p>
+          <h3 key={e.type} name='type'>{e.type}</h3>
+          {props.text!== 'Bebidas' && <img src={e.img} alt='Ícone do item'/>}
+          </div>))}
+        {props.burguer && <BurguerArea burguerClick={props.burguerClick} checkbox={props.checkbox} setCheckbox={props.setCheckbox} setValue={props.setValue}/>}
+      </li>
+    </ul>
   </li>
 )
 
