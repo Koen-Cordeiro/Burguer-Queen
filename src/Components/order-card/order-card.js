a6b52d7434c318a160b3386b2df43e6a6247d92a
Caroline Pinheiro <ommasshi@gmail.com>
2020-08-03 18:34:13 -0300

    key da section atualizada

--- src/Components/order-card/order-card.js
+++ src/Components/order-card/order-card.js
@@ -1,9 +1,9 @@
 import React from 'react'
 const orderCard = ({e,index}) =>{ 
   return (
-    <li key={index}>
+    <li key={index }>
       <p key={e.orderNumber + index}>N°{e.orderNumber}</p>
-      <section>
+      <section key={Math.random() * 30}>
   <p key={e.table+index}>Mesa {e.table}</p>
   <p key={e.waitingTime + index}>Eu{e.waitingTime}</p>
   <p key={e.clientName+index}>Mesa {e.clientName}</p>
