ec0cfe84f67a065e38310f97e1343d1470ba70ae
Caroline Pinheiro <ommasshi@gmail.com>
2020-08-03 18:59:16 -0300

    Css corrigido

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import { render } from 'react-dom'
 import App from './app'
 import './firebase' 
 import './reset.css'
-import './css/style.css'
+import './scss/styles/login-register.scss'
 
 render( <App/>, document.getElementById('root'))
