dc39cfc38ec3e2880d89e66a69741168a63a06ee
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-08-03 16:51:36 -0300

    Aplicação do estilo de título no componente

--- src/Components/menu-items/menu-items.js
+++ src/Components/menu-items/menu-items.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import BurguerArea from '../burguer-area/burguer-area'
+import './menu-items.scss'
+
 const MenuItems = (props) => (
   <li>
-  <h1>{props.text}</h1>
-      {props.arr.map((e,index) =>( 
+    <h2 className='title'>{props.text}</h2>
+    {props.arr.map((e,index) =>( 
     <div key={index} onClick={props.handleClick}>
     {props.text!== 'Bebidas' && <img src={e.img} alt='Imagem do item do menu'/>}
     <p>R$<span key={e.price}>{e.price}</span></p>
