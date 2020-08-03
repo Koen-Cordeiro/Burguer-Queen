13e8ddf5b429d525150cb0837d6cecf9ba9ac8b3
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-08-03 11:09:54 -0300

    Condição de leitura da imagem no campo de bebidas

--- src/Components/menu-items/menu-items.js
+++ src/Components/menu-items/menu-items.js
@@ -5,7 +5,7 @@ const MenuItems = (props) => (
   <h1>{props.text}</h1>
       {props.arr.map((e,index) =>( 
     <div key={index} onClick={props.handleClick}>
-    <img src={e.img} alt='Imagem do item do menu'/>
+    {props.text!== 'Bebidas' && <img src={e.img} alt='Imagem do item do menu'/>}
     <p>R$<span key={e.price}>{e.price}</span></p>
     <div key={e.type} name='type'>{e.type}</div>
   </div>))}
