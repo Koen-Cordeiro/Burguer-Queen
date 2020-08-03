e0f02731e405b7c32f6898fe7bf60e1ccf000990
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-08-03 14:07:54 -0300

    Criação e estilização da sidebar

--- src/Pages/saloon/saloon.js
+++ src/Pages/saloon/saloon.js
@@ -3,7 +3,7 @@ import Menu from './menu'
 import Orders from './orders'
 import Nav from '../../Components/nav/nav'
 import Logo from '../../Components/logo/logo'
-
+import '../../scss/styles/saloon.scss'
 
 const Saloon = () => {
   const [menu, setMenu] = useState(true)
@@ -14,12 +14,14 @@ const Saloon = () => {
   ];
 
   return (
-    <aside>
-      <Logo use='sidebar'/>
-      <Nav use='sidebar' arr={arrMenu}/>
+    <div className='frame'>
+      <aside className='sidebar'>
+        <Logo use='sidebar'/>
+        <Nav use='sidebar' arr={arrMenu}/>
+      </aside>
       {menu && <Menu />}
       {!menu && <Orders />}
-    </aside>
+    </div>
   );
 };
 
