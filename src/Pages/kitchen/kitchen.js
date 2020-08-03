7f7796f4c02314f22afadb3447345ed8fcbcf29e
Caroline Pinheiro <ommasshi@gmail.com>
2020-08-03 18:33:57 -0300

    Tempo atualiza dinamicamente agora

--- src/Pages/kitchen/kitchen.js
+++ src/Pages/kitchen/kitchen.js
@@ -6,21 +6,18 @@ const Kitchen = () => {
   const [open, setOpen] = useState([])
   const [date, setDate] = useState(new Date().getTime())
 
-  // setInterval(()=> setDate(new Date().getTime()), 4000 )
-
-  useEffect(()=> console.log(open), [open])
+  setInterval(() => setDate(new Date().getTime()), 60000)
 
   useEffect(() => {
     firebase.firestore().collection('orders').onSnapshot((snap => {
       const getBurguer = snap.docs.map((doc) => ({
         id: doc.id,
-        waitingTime:  (((date - doc.data().msOrdered)/1000)/60).toFixed(0),
+        waitingTime: Number((((date - doc.data().msOrdered) / 1000) / 60).toFixed(0)),
         ...doc.data()
       }))
       setOpen(() => getBurguer);
     }))
-  }, [])
- // ((new Date().getTime() - ms)/1000)/60
+  }, [date])
   return (
     <>
       <h1>Bem vindo rei do masterchef</h1>
