7403e16de0e7cdf390b20002bb247c4bc5ba116e
Caroline Pinheiro <ommasshi@gmail.com>
2020-08-03 17:32:43 -0300

    Versão inicial dos pedidos da cozinha criados

--- src/Pages/kitchen/kitchen.js
+++ src/Pages/kitchen/kitchen.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import firebase from 'firebase'
+import OrderCard from '../../Components/order-card/order-card'
 
 const Kitchen = () => {
+  const [open, setOpen] = useState([])
+  const [date, setDate] = useState(new Date().getTime())
+
+  // setInterval(()=> setDate(new Date().getTime()), 4000 )
+
+  useEffect(()=> console.log(open), [open])
+
+  useEffect(() => {
+    firebase.firestore().collection('orders').onSnapshot((snap => {
+      const getBurguer = snap.docs.map((doc) => ({
+        id: doc.id,
+        waitingTime:  (((date - doc.data().msOrdered)/1000)/60).toFixed(0),
+        ...doc.data()
+      }))
+      setOpen(() => getBurguer);
+    }))
+  }, [])
+ // ((new Date().getTime() - ms)/1000)/60
   return (
-    <main>
+    <>
       <h1>Bem vindo rei do masterchef</h1>
       <button onClick={() => firebase.auth().signOut()}> Sair</button>
-    </main>
+      <ul>
+        {open.map((e, index) => {
+          return (<OrderCard e={e} index={index} key={index + 3000} />)
+        })}
+
+      </ul>
+    </>
   );
 };
 
