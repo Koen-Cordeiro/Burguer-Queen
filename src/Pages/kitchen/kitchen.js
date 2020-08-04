4ce08f13a7b78fc01a7d6b4f9fa544711ae3a47c
Caroline Pinheiro <ommasshi@gmail.com>
2020-08-04 18:03:58 -0300

    Botões para alteração dos estados dos pedidos

--- src/Pages/kitchen/kitchen.js
+++ src/Pages/kitchen/kitchen.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import firebase from 'firebase'
 import CardBoard from '../../Containers/cardboard/card-board'
+import Button from '../../Components/button/button'
 const Kitchen = () => {
   const [open, setOpen] = useState([])
+  const [pending, setPending] = useState([])
+  const [doing, setDoing] = useState([])
+  const [delivered, setDelivered] = useState([])
   const [date, setDate] = useState(new Date().getTime())
 
   setInterval(() => setDate(new Date().getTime()), 60000)
@@ -11,16 +15,24 @@ const Kitchen = () => {
     firebase.firestore().collection('orders').onSnapshot((snap => {
       const getBurguer = snap.docs.map((doc) => ({
         id: doc.id,
-        waitingTime: Number((((date - doc.data().msOrdered) / 1000) / 60).toFixed(0)),
+        waitingTime: Number((((new Date().getTime() - doc.data().msOrdered) / 1000) / 60).toFixed(0)),
         ...doc.data()
       }))
-      setOpen(() => getBurguer);
+      setPending(getBurguer.filter(e => e.orderStatus === 'pending'))
+      setDoing(getBurguer.filter(e => e.orderStatus === 'doing'))
+      setDelivered(getBurguer.filter(e => e.orderStatus === 'delivered'))
     }))
   }, [date])
+
+  // useEffect(()=> , [])
+
   return (
     <>
-      <button onClick={() => firebase.auth().signOut()}> Sair</button>
-      <CardBoard arr={open}/>
+      <Button text='Sair' handleClick={() => firebase.auth().signOut()} />
+      <Button text='Abertos' handleClick={() => setOpen(pending)} />
+      <Button text='Prontos' handleClick={() => setOpen(doing)} />
+      <Button text='Entregues' handleClick={() => setOpen(delivered)} />
+      <CardBoard arr={open} />
     </>
   );
 };
