cbe8ccae035fc366597029d98af1c6659288a8e6
Caroline Pinheiro <ommasshi@gmail.com>
2020-08-04 10:50:13 -0300

    Css básico criado para funcionamento do flex-wrap e melhor visualização dos cards

--- src/Containers/cardboard/card-board.js
+++ src/Containers/cardboard/card-board.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './card-board.scss'
 import OrderCard from '../../Components/order-card/order-card'
 
 
 const CardBoard = ({arr}) => (
-  <ul>
+  <ul className='cardboard-flex'>
   {arr.map((e, index) => {
     return (<OrderCard e={e} index={index} key={index + 3000} />)
   })}
