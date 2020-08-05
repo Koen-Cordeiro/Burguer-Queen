e88662e183d41c34ce3e4e530a1ad3a1d328a460
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-08-05 15:22:25 -0300

    Estilização do componente MenuItems

--- src/Components/burguer-area/burguer-area.js
+++ src/Components/burguer-area/burguer-area.js
@@ -11,27 +11,33 @@ const setExtras = (e) => {
 
   return (
     <div className='options'>
-      <label className='label-burguer'>{props.labelTextOne}
-      <div onChange={event => props.setValue(event.target.value)} className='input-radio'>
-        <Input type='radio' value={props.textInputOne} name={'radio'} textRadio={props.textInputOne} />
-        <Input type='radio' value={props.textInputTwo} name={'radio'} textRadio={props.textInputTwo} />
-        <Input type='radio' value={props.textInputThree} name={'radio'} textRadio={props.textInputThree} />
-      </div>
-      <label className='label-burguer'>Extras
-      <Input textRadio={props.extraOne} name={props.extraOne} handleClick={(e)=> setExtras(e)} type='Checkbox' />
-      <Input textRadio={props.extraTwo} name={props.extraTwo}  handleClick={(e)=> setExtras(e)} type='Checkbox'/>
-      </label>
+      <section className='item__burguer'>
+        {props.titleTextOne}
+        <div onChange={event => props.setValue(event.target.value)} className='item__burguer--meat'>
+          <Input use='burguer' specific='item' type='radio' value={props.textInputOne} name={'radio'} textRadio={props.textInputOne} />
+          <Input use='burguer' specific='item' type='radio' value={props.textInputTwo} name={'radio'} textRadio={props.textInputTwo} />
+          <Input use='burguer' specific='item' type='radio' value={props.textInputThree} name={'radio'} textRadio={props.textInputThree} />
+        </div>
+      </section>
+      <section className='item__extra'>
+        {props.titleTextTwo}{"\u00a0"}<span className='color'>•</span>{"\u00a0"}R${"\u00a0"}{props.price}{"\u00a0"}cada
+        <div className='item__extra--choice'>
+          <Input use='burguer' specific='item' type='checkbox' textRadio={props.extraOne} name={props.extraOne} handleClick={(e)=> setExtras(e)}  />
+          <Input use='burguer' specific='item' type='checkbox' textRadio={props.extraTwo} name={props.extraTwo}  handleClick={(e)=> setExtras(e)} />
+        </div>
+      </section>
       <Button text='Adicionar' handleClick={props.burguerClick}/>
-      </label>
     </div>
   )
 }
 
 MenuItems.defaultProps = {
+  titleTextOne: 'Hambúrguer',
   textInputOne: 'Bovino',
   textInputTwo: 'Frango',
   textInputThree: 'Vegano',
-  labelTextOne: 'Hambúrguer',
+  titleTextTwo: 'Extras',
+  price: '1',
   extraOne: 'Ovo',
   extraTwo: 'Queijo'
 }
