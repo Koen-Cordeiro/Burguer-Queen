66af36daf6689e1abfa63ee7eb139d0092762042
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-08-05 19:20:00 -0300

    Remoção do texto dos inputs e transferência para o content no estilo do CSS

--- src/Components/burguer-area/burguer-area.js
+++ src/Components/burguer-area/burguer-area.js
@@ -14,32 +14,27 @@ const setExtras = (e) => {
       <section className='item__burguer'>
         {props.titleTextOne}
         <div onChange={event => props.setValue(event.target.value)} className='item__burguer--meat'>
-          <Input use='burguer' specific='item' type='radio' value={props.textInputOne} name={'radio'} textRadio={props.textInputOne} />
-          <Input use='burguer' specific='item' type='radio' value={props.textInputTwo} name={'radio'} textRadio={props.textInputTwo} />
-          <Input use='burguer' specific='item' type='radio' value={props.textInputThree} name={'radio'} textRadio={props.textInputThree} />
+          <Input use='burguer' specific='item burguer--text--bovino' type='radio' value={props.textInputOne} name={'radio'} />
+          <Input use='burguer' specific='item burguer--text--frango' type='radio' value={props.textInputTwo} name={'radio'} />
+          <Input use='burguer' specific='item burguer--text--vegano' type='radio' value={props.textInputThree} name={'radio'} />
         </div>
       </section>
       <section className='item__extra'>
         {props.titleTextTwo}{"\u00a0"}<span className='color'>•</span>{"\u00a0"}R${"\u00a0"}{props.price}{"\u00a0"}cada
         <div className='item__extra--choice'>
-          <Input use='burguer' specific='item' type='checkbox' textRadio={props.extraOne} name={props.extraOne} handleClick={(e)=> setExtras(e)}  />
-          <Input use='burguer' specific='item' type='checkbox' textRadio={props.extraTwo} name={props.extraTwo}  handleClick={(e)=> setExtras(e)} />
+          <Input use='burguer' specific='item extra--text--ovo' type='checkbox' name={props.extraOne} handleClick={(e)=> setExtras(e)}  />
+          <Input use='burguer' specific='item extra--text--queijo' type='checkbox' name={props.extraTwo}  handleClick={(e)=> setExtras(e)} />
         </div>
       </section>
-      <Button text='Adicionar' handleClick={props.burguerClick}/>
+      <Button type={'burguer'} handleClick={props.burguerClick}/>
     </div>
   )
 }
 
 MenuItems.defaultProps = {
   titleTextOne: 'Hambúrguer',
-  textInputOne: 'Bovino',
-  textInputTwo: 'Frango',
-  textInputThree: 'Vegano',
   titleTextTwo: 'Extras',
   price: '1',
-  extraOne: 'Ovo',
-  extraTwo: 'Queijo'
 }
 
 export default MenuItems
