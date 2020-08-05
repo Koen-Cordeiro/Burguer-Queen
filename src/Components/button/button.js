7db2ea0274150eb7b1347443702c560baecf040f
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-08-05 09:08:43 -0300

    Refatoração estilos de button e nav

--- src/Components/button/button.js
+++ src/Components/button/button.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './button.scss'
 
 const Button = (props) => {
-  const buttonClass = `button-${props.type}`
+  const buttonClass = `button_${props.type}`
   return(<button className={buttonClass} onClick={props.handleClick}>
     {props.text}
   </button>)
