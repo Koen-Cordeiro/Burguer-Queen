7505b62c2b1daab70b0e346ac0dd923f31237757
Caroline Pinheiro <ommasshi@gmail.com>
2020-08-05 09:51:21 -0300

    Agora a área de entregues tem todos os pedidos e eles não atualizam dinâmicamente como as outras áreas

--- src/Pages/kitchen/kitchen.js
+++ src/Pages/kitchen/kitchen.js
@@ -6,6 +6,7 @@ const Kitchen = () => {
   const [open, setOpen] = useState([])
   const [status, setStatus] = useState('pending')
   const [date, setDate] = useState(new Date().getTime())
+  const [delivered, setDelivered] = useState([])
 
   setInterval(() => setDate(new Date().getTime()), 60000)
 
@@ -16,7 +17,7 @@ const Kitchen = () => {
         waitingTime: Number((((new Date().getTime() - doc.data().msOrdered) / 1000) / 60).toFixed(0)),
         ...doc.data()
       }))
-      
+      setDelivered(getData.filter(e=> e.orderStatus === 'delivered'))
       setOpen(getData)
     }))
   }, [])
@@ -28,8 +29,9 @@ const Kitchen = () => {
       <Button text='Sair' handleClick={() => firebase.auth().signOut()} />
       <Button text='Abertos' handleClick={() => setStatus('pending')} />
       <Button text='Prontos' handleClick={() => setStatus('doing')} />
-      <Button text='Entregues' handleClick={() => setStatus('delivered')} />
-      <CardBoard arr={open.filter(e=> e.orderStatus === status)} />
+      <Button text='Entregues' handleClick={() => setStatus('')} />
+      {status.length>0 && <CardBoard arr={open.filter(e=> e.orderStatus === status)} />}
+      {status.length===0 && <CardBoard arr={delivered} />}
     </>
   );
 };
