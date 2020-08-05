a601288b871e4ea5423eb471cfbdeb161ba77559
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-08-05 10:28:43 -0300

    Aplicação de estilos diretos no componente

--- src/Components/burguer-area/burguer-area.js
+++ src/Components/burguer-area/burguer-area.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Input from '../input/input'
 import Button from '../button/button'
+import './burguer-area.scss'
 
 const MenuItems = (props) => {
 const setExtras = (e) => {
@@ -9,18 +10,20 @@ const setExtras = (e) => {
 }
 
   return (
-    <label className='reg-work-label log-reg-space-after'>{props.labelText}
+    <div className='options'>
+      <label className='label-burguer'>{props.labelTextOne}
       <div onChange={event => props.setValue(event.target.value)} className='input-radio'>
         <Input type='radio' value={props.textInputOne} name={'radio'} textRadio={props.textInputOne} />
         <Input type='radio' value={props.textInputTwo} name={'radio'} textRadio={props.textInputTwo} />
         <Input type='radio' value={props.textInputThree} name={'radio'} textRadio={props.textInputThree} />
       </div>
-      <label className='reg-work-label log-reg-space-after'>Extras
+      <label className='label-burguer'>Extras
       <Input textRadio={props.extraOne} name={props.extraOne} handleClick={(e)=> setExtras(e)} type='Checkbox' />
       <Input textRadio={props.extraTwo} name={props.extraTwo}  handleClick={(e)=> setExtras(e)} type='Checkbox'/>
       </label>
-    <Button text='Adicionar' handleClick={props.burguerClick}/>
-    </label>
+      <Button text='Adicionar' handleClick={props.burguerClick}/>
+      </label>
+    </div>
   )
 }
 
@@ -28,7 +31,7 @@ MenuItems.defaultProps = {
   textInputOne: 'Bovino',
   textInputTwo: 'Frango',
   textInputThree: 'Vegano',
-  labelText: 'Escolha o tipo da carne',
+  labelTextOne: 'Hambúrguer',
   extraOne: 'Ovo',
   extraTwo: 'Queijo'
 }
