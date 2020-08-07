900282d9cbe85a81598dc93d1742f74abda0321e
Caroline Pinheiro <ommasshi@gmail.com>
2020-08-07 07:29:26 -0300

    Classes no botão de aceite e na mensagem de entregues criada

--- src/Components/order-card/order-card.js
+++ src/Components/order-card/order-card.js
@@ -44,8 +44,8 @@ const orderCard = ({ e, index }) => {
       </section>
       <div className='order-card-div-bottom'>
         {e.orderStatus === 'pending' && window.location.pathname === '/kitchen' && <Button key={`accept-${index}`} type='accept' text='Aceitar' handleClick={async () => await firebase.firestore().collection('orders').doc(e.id).update({ orderStatus: 'doing' })} />}
-        {e.orderStatus === 'doing' && window.location.pathname === '/kitchen' && <Button key={`finalize-${index}`} text='Finalizar' handleClick={async () => await firebase.firestore().collection('orders').doc(e.id).update({ orderStatus: 'ready' })} />}
-        {e.orderStatus === 'delivered' && window.location.pathname === '/kitchen' && <p key={`delivered-${index}`}>Entregue</p>}
+        {e.orderStatus === 'doing' && window.location.pathname === '/kitchen' && <Button key={`finalize-${index}`} type='accept' text='Finalizar' handleClick={async () => await firebase.firestore().collection('orders').doc(e.id).update({ orderStatus: 'ready' })} />}
+        {e.orderStatus === 'delivered' && window.location.pathname === '/kitchen' && <p key={`delivered-${index}`} className='order-card__read'>Entregue</p>}
       </div>
     </li>
 
