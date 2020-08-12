bd3c61e49b9a47db86c7b69265fddbfbd22e9beb
Caroline Pinheiro <ommasshi@gmail.com>
2020-08-12 10:55:56 -0300

    Erro do displayName corrigido

--- src/Pages/kitchen/kitchen.js
+++ src/Pages/kitchen/kitchen.js
@@ -13,9 +13,9 @@ const Kitchen = () => {
   const [date, setDate] = useState(new Date().getTime())
   const [delivered, setDelivered] = useState([])
 
-const interval = setInterval(() => setDate(new Date().getTime()), 60000)
+  const interval = setInterval(() => setDate(new Date().getTime()), 60000)
 
-  
+
 
   useEffect(() => {
     const orders = firebase.firestore().collection('orders').onSnapshot(snap => {
@@ -24,43 +24,44 @@ const interval = setInterval(() => setDate(new Date().getTime()), 60000)
         waitingTime: Number((((new Date().getTime() - doc.data().msOrdered) / 1000) / 60).toFixed(0)),
         ...doc.data()
       }))
-      setDelivered(getData.filter(e=> e.orderStatus === 'delivered'))
+      setDelivered(getData.filter(e => e.orderStatus === 'delivered'))
       setOpen(getData)
     })
     return orders
   }, [])
 
-  useEffect(()=> setOpen(o => o.map(e => ({...e, waitingTime: Number((((new Date().getTime() - e.msOrdered) / 1000) / 60).toFixed(0)) }))) , [date])
+  useEffect(() => setOpen(o => o.map(e => ({ ...e, waitingTime: Number((((new Date().getTime() - e.msOrdered) / 1000) / 60).toFixed(0)) }))), [date])
 
   const arrMenu = [
-    {menuText:'Abertos', menuClass:status === 'pending'? 'sidebar active space' : 'sidebar space', menuClick:() => setStatus('pending')},
-    {menuText:'Prontos', menuClass:status === 'doing' ? 'sidebar active space' : 'sidebar space', menuClick:() => setStatus('doing')},
-    {menuText:'Entregues', menuClass:status === '' ? 'sidebar active space' : 'sidebar space', menuClick:() => setStatus('')},
+    { menuText: 'Abertos', menuClass: status === 'pending' ? 'sidebar active space' : 'sidebar space', menuClick: () => setStatus('pending') },
+    { menuText: 'Prontos', menuClass: status === 'doing' ? 'sidebar active space' : 'sidebar space', menuClick: () => setStatus('doing') },
+    { menuText: 'Entregues', menuClass: status === '' ? 'sidebar active space' : 'sidebar space', menuClick: () => setStatus('') },
   ];
 
   return (
     <div className='frame'>
       <aside className='sidebar'>
-        <Logo use='sidebar'/>
-        <Nav use='sidebar' arr={arrMenu}/>
+        <Logo use='sidebar' />
+        <Nav use='sidebar' arr={arrMenu} />
         <p className='copyright__land'>© Caroline Pinheiro e Tamires Cordeiro</p>
       </aside>
       <section className='order__cards order__cards-kitchen'>
         <header className='order__top'>
-          <h1>{firebase.auth().currentUser.displayName}</h1>
-          <Button type='logout--gray icon-door' text='Sair' handleClick={() =>{ 
+          {firebase.auth().currentUser !== null && <h1>{firebase.auth().currentUser.displayName}</h1>}
+          <Button type='logout--gray icon-door' text='Sair' handleClick={() => {
             clearInterval(interval)
-            firebase.auth().signOut()}} />
+            firebase.auth().signOut()
+          }} />
         </header>
-        {status.length>0 && <CardBoard arr={open.filter(e=> e.orderStatus === status)} />}
-        {status.length===0 && <CardBoard arr={delivered} />}
+        {status.length > 0 && <CardBoard arr={open.filter(e => e.orderStatus === status)} />}
+        {status.length === 0 && <CardBoard arr={delivered} />}
         <footer className='copyright__portrait'>
           © Caroline Pinheiro e Tamires Cordeiro
         </footer>
       </section>
     </div>
 
-    
+
   );
 };
 
